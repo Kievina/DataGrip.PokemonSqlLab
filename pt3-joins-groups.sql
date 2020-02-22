f24a9fccd5cdcf791c42ab0031d69600bf363b92
kievina <kievina@github.com>
2020-02-22 07:21:36 -0500

    complete first two sql statements in pt3

--- pt3-joins-groups.sql
+++ pt3-joins-groups.sql
@@ -1,6 +1,20 @@
 /* What is each pokemon's primary type? */
+select pkm.name as name, t.name as primary_type
+from pokemon.pokemons pkm
+left outer join pokemon.types t
+on pkm.primary_type = t.id;
+
 
 /* What is Rufflet's secondary type? */
+select pkm.name, pkm.secondary_type
+from pokemon.pokemons pkm
+where pkm.name = "Rufflet";
+
+select pkm.name as pokemon_name, t.name as secondary_type
+from pokemon.pokemons pkm join pokemon.types t
+on pkm.secondary_type = t.id
+where pkm.name = "Rufflet";
+
 /* What are the names of the pokemon that belong to the trainer with trainerID 303? */
 /* How many pokemon have a secondary type Poison */
 /* What are all the primary types and how many pokemon have that type? */
