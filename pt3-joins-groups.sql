8e9701287e1cfd43c1b7762224c6ff9edc59a098
kievina <kievina@github.com>
2020-02-27 23:02:59 -0500

    add two queries to pt3

--- pt3-joins-groups.sql
+++ pt3-joins-groups.sql
@@ -16,7 +16,31 @@ on pkm.secondary_type = t.id
 where pkm.name = "Rufflet";
 
 /* What are the names of the pokemon that belong to the trainer with trainerID 303? */
+select train.trainerID, pkm.name as pokemon_name
+from pokemon.pokemon_trainer as train
+join pokemon.pokemons as pkm
+on train.pokemon_id = pkm.id
+where train.trainerID = 303;
+
+select distinct pkm.name as pokemon_name
+from pokemon.pokemon_trainer as train
+join pokemon.pokemons as pkm
+on train.pokemon_id = pkm.id
+where train.trainerID = 303;
+
 /* How many pokemon have a secondary type Poison */
+select pkm.secondary_type
+from pokemon.pokemons pkm
+left join pokemon.types t
+on pkm.secondary_type = t.id
+where t.name = "Poison";
+
+select count(t.name)
+from pokemon.pokemons pkm
+left join pokemon.types t
+on pkm.secondary_type = t.id
+where t.name = "Poison";
+
 /* What are all the primary types and how many pokemon have that type? */
 /* How many pokemon at level 100 does each trainer with at least one level 100 pokemon have? (Hint: your query should not display a trainer) */
 /* How many pokemon only belong to one trainer and no other? */
